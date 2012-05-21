addea6315c4488c227e9ec97e15e52363b2097f8
Josef <github.com@netpage.dk>
2012-05-21 04:18:59 +0200

    read worts to array

--- src/eu/j0s/euler/p42/Main.java
+++ src/eu/j0s/euler/p42/Main.java
@@ -1,5 +1,52 @@
 package eu.j0s.euler.p42;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Vector;
 
+public class Main {
+	public static void main(String [ ] args)
+	{
+		Main obj = new Main();
+		try {
+			for (String str : obj.readWordlist()){
+				System.out.println(str);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private  String[] wordList;
+
+	public synchronized String[] readWordlist() throws IOException {
+		if (wordList == null) {
+			Vector<String> vec = new Vector<String>();
+			InputStream in = this.getClass().getResourceAsStream(
+					"/res/words.txt");
+			if (in == null)
+				throw new IOException("resource 'words.txt' not found!");
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+
+			while ((strLine = br.readLine()) != null) {
+				String words[] = strLine.split(",");
+				for (String word : words) {
+					vec.add(word.substring(word.indexOf('"')+1,word.lastIndexOf('"')));
+				}
+			}
+
+			in.close();
+			this.wordList = vec.toArray(new String[0]);
+		}
+		return wordList;
+	}
 }
